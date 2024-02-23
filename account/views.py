a080aafe5fe6d35cc4d6e916718922d5e7a4ae25
Sharad Rao <sharad011989@gmail.com>
2024-02-23 07:34:22 +0530

    add

--- account/views.py
+++ account/views.py
@@ -1,8 +1,19 @@
+from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 
 from .models import User
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+
+        if email and password:
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                auth_login(request, user)
+
+                return redirect('/')
     return render(request, 'account/login.html')
 
 def signup(request):
--- pizarra/urls.py
+++ pizarra/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('account.urls')),
+    path('projects/', include('project.urls')),
 ]
