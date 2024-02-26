d82347ef784628f2e014cf8798449bfce4a221cc
Sharad Rao <sharad011989@gmail.com>
2024-02-26 13:51:05 +0530

    add

--- project/views.py
+++ project/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render, redirect
 
 from .models import Project
 
-@login_required
+#@login_required
 def projects(request):
     projects = Project.objects.filter(created_by=request.user)
     return render(request, 'project/projects.html', {
         'projects': projects
     })
 
-@login_required
+#@login_required
 def project(request, pk):
     project = Project.objects.filter(created_by=request.user).get(pk=pk)
 
@@ -18,7 +18,7 @@ def project(request, pk):
         'project': project
     })
 
-@login_required
+#@login_required
 def add_project(request):
     if request.method == 'POST':
         name = request.POST.get('name', '')
